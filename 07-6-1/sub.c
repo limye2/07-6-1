b88888ca1b5ef7b27506e0d2c63495bd7476b02d
limye2 <144992260+limye2@users.noreply.github.com>
2023-10-19 16:21:23 +0900

    Initial Commit

---
+++ 07-6-1/sub.c
@@ -0,0 +1,14 @@
+//
+//  sub.c
+//  07-6-1
+//
+//  Created by MacBook Air on 2023/10/19.
+//
+
+#include "sub.h"
+
+extern int all_files;
+void sub(void)
+{
+    all_files = 10;
+}
---
+++ 07-6-1/sub.h
@@ -0,0 +1,13 @@
+//
+//  sub.h
+//  07-6-1
+//
+//  Created by MacBook Air on 2023/10/19.
+//
+
+#ifndef sub_h
+#define sub_h
+
+#include <stdio.h>
+
+#endif /* sub_h */
